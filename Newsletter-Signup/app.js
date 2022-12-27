e0963f2a43f06753609c9acd52f341b02cf85427
Jona Hyun <jona.hyun@gmail.com>
2022-12-27 00:32:42 -0500

    Newsletter signup

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,16 +1,79 @@
-const express = require("express");
+const express = require("express"); 
 const app = express();
-const bodyParser = require("body-parser");
-const https = require("https");
+const bodyParser = require("body-parser"); // use to post form data
+const https = require("https"); 
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true})); // For nested 
+app.use(express.static("public")); //allow to use static css and images folder
 
+
+// Route to Main page
 app.get("/", function(req, res) {
-  res.send("You are here");
+  res.sendFile(__dirname + "/signup.html");
 });
+
+// Handle form post request
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  
+  // JSON Data format
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  // string data format to send to Mailchimp
+  const jsonData = JSON.stringify(data);
+
+  // us8 is specific to user and list ID at the end
+  const url = "https://us8.api.mailchimp.com/3.0/lists/5f23e46090";
+
+  const options = {
+    method: "POST",
+    auth: "jona:b10d3a98f29e9a802fa60ee057c19304-us8"
+  }
+
+  const request = https.request(url, options, function(response) {
+    
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    };
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+    
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+
+})
 
 app.listen(3000, function(req, res){
   console.log("The server is alive on 3000");
 });
 
-// 676d40b35b02c0a5470d0b93c7d653de-us8
+// API Key
+// b10d3a98f29e9a802fa60ee057c19304-us8
+
+
+// List ID
+// 5f23e46090
