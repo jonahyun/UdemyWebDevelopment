5ee368f278eeaa657f9ad762c7f6dabe381707ec
Jona Hyun <jona.hyun@gmail.com>
2022-12-27 11:43:31 -0500

    added Procfile for Heroku to tell them how/where to launch our server file

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -70,7 +70,7 @@ app.post("/failure", function(req, res) {
 
 })
 
-app.listen(3000, function(req, res){
+app.listen(process.env.PORT || 3000, function(req, res){
   console.log("The server is alive on 3000");
 });
 
