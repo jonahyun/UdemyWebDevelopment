ddf0e517afb57726cb1ae9dfd9b3f2803899d7ec
Jona Hyun <jona-air@jonas-air.lan>
2022-12-29 13:18:53 -0500

    creating and calling custom module date.js

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,32 +1,42 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const getDate = require('./date');
 const app = express();
+const date = require(__dirname + "/date.js");
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-var items = ["Buy Food", "Cook Food", "Eat"];
+let items = ["Buy Food", "Cook Food", "Eat"];
+let workItems = ["programming", "fixing house"];
 
 app.get("/", function(req, res){
   // res.sendFile(__dirname + "/index.html");
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay: day, newListItems: items}); //render - ejs function
+  let day = date.getDay();
+  res.render("list", {listTitle: day, newListItems: items}); //render - ejs function
 });
 
 app.post("/", function(req, res){
-  var item = req.body.newItem;
-  items.push(item);
 
-  res.redirect("/"); // the newListItem must be inside app.get to be rendered.
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work", newListItems: workItems});
+});
+
+app.get("/about", function(req,res){
+  res.render("about");
+})
 
 app.listen(3000, function(req, res){
   console.log("The server is running on 3000");
