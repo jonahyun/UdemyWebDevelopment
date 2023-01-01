f28c93d72272276156e17056f02f4eacfa326e9e
Jona Hyun <jona.hyun@gmail.com>
2023-01-01 12:00:55 -0500

    pracctice ejs challenge 2

--- ejs-challenge2/app.js
+++ ejs-challenge2/app.js
@@ -10,75 +10,60 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+posts = [];
 
-app.get("/", function(req, res) {
+
+app.get("/", function(req,res){
   res.render("home", {
-    homeStartingContent:homeStartingContent, 
+    homeStartingContent:homeStartingContent,
     posts:posts
   });
-
+ 
 });
 
-
-
-app.get("/about", function(req, res) {
+app.get("/about",function(req,res) {
   res.render("about", {aboutContent:aboutContent});
 });
 
-app.get("/contact", function(req, res) {
+app.get("/contact",function(req,res){
   res.render("contact", {contactContent:contactContent});
 });
 
-app.get("/compose", function(req,res){
+app.get("/compose", function(req,res) {
   res.render("compose");
-})
+});
 
-app.post("/compose", function(req,res) {
-  let post = { 
-    title : req.body.postTitle, 
-    content : req.body.postBody
+app.post("/compose", function(req,res){
+  const post= {
+    title: req.body.postTitle,
+    content: req.body.postBody
   };
- 
   posts.push(post);
   res.redirect("/");
+
 });
 
-
-
-// dynamic website 
-app.get("/posts/:postName", function(req, res) {
-
+app.get("/posts/:postName", function(req, res){
   const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(post) {
+ 
+  posts.forEach(function(post){
     const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      
-      res.render("post", { 
-        title : post.title, 
-        content : post.content
+    if (requestedTitle === storedTitle) {
+      res.render("post", {
+        title:post.title,
+        content:post.content
       });
-
-    };
-  });
-
-  // for (var i = 0; i < posts.length;i++) {
-  //   if (posts[i].title === requestedTitle) {
-  //     console.log("matched Something")
-  //   };
-  // };
-
-
+    }
+  })
 
 });
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
